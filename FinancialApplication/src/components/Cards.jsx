4a724e72ba5566ea3d3ecc25853e008d89c05c4a
Brandon Lara <larabrandon408@gmail.com>
2024-02-25 12:10:18 -0800

    Edited cards

--- FinancialApplication/src/components/Cards.jsx
+++ FinancialApplication/src/components/Cards.jsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom'
 import { FaHouse } from "react-icons/fa6"
 import './Cards.css'
 
-const Card = ({labelText, headingText}) => {
+const Card = ({labelText, headingText,path, image}) => {
     return (
-        <div className="card">]
-            <img src />
+    <Link to={path} className="card-link">
+        <div className="card">
+            <div className="image-container">
+                <img src={image} alt="Card Image" />
+            </div>
             <label htmlFor='text'>{labelText}</label>
             <h2>{headingText}</h2>
-
         </div>
+    </Link>
+        
     );
 };
 
--- FinancialApplication/src/pages/Home.jsx
+++ FinancialApplication/src/pages/Home.jsx
@@ -7,23 +7,25 @@ import { FaPiggyBank } from "react-icons/fa6";
 import { FaArrowUpFromGroundWater } from "react-icons/fa6";
 import Card from '../components/Cards';
 import Document from '../components/Document';
+import savingImage from '../assets/savings.jpg'
 import "./Home.css"
 const HomePage = () => {
     return (
         <div>
             <h1>Home</h1>
-            <Card labelText={"Investign 101"}></Card>
-            <Document labelText={"Investing 101"} 
-            paragraphText={`lorem ajusdbiha as dijabwhiud aw d awd awnawdssasddddddddddddddddddddddd
-            ddddddddddddddddddddddddddddddddddddddddddddddddddds
-            sdddjanjicnaejicnjiencoienconeiocnwencijowenconweocnwe
-            ecwjiencjiwen cji wejic wje cjiwe cijw ecij wejic we
-            cmowencowe cojw eojc weoj cojwe coj weoc owje cjowec
-             jowe cowe ocj wejoc jowe cjiwe cjiw e
-             we jc qo cokq cwe 0 qo vojwe ovj ewjov weojmoqwdmi0q
-             qnwodiqn wodnqwdnoqwndoqndioqd`} 
-            headingText={"How to get into investing"}>
-            </Document>
+            <Card 
+            labelText={"Learn How to Save"}
+            headingText={"click to learn now"}
+            path={"/savings"}
+            image={savingImage}
+            />
+            <Card 
+            labelText={"Learn How to Save"}
+            headingText={"click to learn now"}
+            path={"/savings"}
+            image={savingImage}
+            />
+            <Card labelText={"Get started on the path to financial freedom"}></Card>
             <div className = "icon-container">
                 <Link to="/home"><FaHouse size={32} color = "lightblue" /></Link>
                 <Link to="/investment"><FaArrowTrendUp size={32}color = "lightblue"/></Link>
